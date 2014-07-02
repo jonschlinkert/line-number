b1da98ad441683126313c017ff892ba1cefff774
jonschlinkert <dev@sellside.com>
2014-07-02 01:05:23 -0400

    use map

--- index.js
+++ index.js
@@ -8,16 +8,9 @@
 'use strict';
 
 module.exports = function lineNo(str, re) {
-  var lines = str.split(/[\r\n]/);
-  var matches = [];
-
-  lines.forEach(function(line, i) {
+  return str.split(/[\r\n]/).map(function(line, i) {
     if (re.test(line)) {
-      matches.push({
-        line: i + 1,
-        match: line.match(re)[0]
-      });
+      return {line: i + 1, match: line.match(re)[0]};
     }
-  });
-  return matches;
+  }).filter(Boolean);
 };
